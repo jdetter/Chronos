48e2f723c33dc1c9291234fc0ca4b2c07ddab80c
Mike Chargo <mike.chargo@sbcglobal.net>
2015-05-20 20:21:33 -0500

    Implementation of some stdlib.c functions

--- include/stdlib.c
+++ include/stdlib.c
@@ -11,27 +11,80 @@ uint strlen(char* str)
 
 void tolower(char* str)
 {
-
+	int x;
+	for(x=0; x < strlen(str); x++)
+	{
+		if(str[x]>='Z' && str[x]<='A')
+		{
+			str[x] = str[x] + 32;
+		}
+	}
 }
 
 void toupper(char* str)
 {
-
+	int x;
+	for(x=0; x < strlen(str); x++)
+	{
+		if(str[x]>='z' && str[x]<='a')
+		{
+			str[x] = str[x] - 32;
+		}
+	}
 }
 
 uint strncpy(char* dst, char* src, uint sz)
 {
-	return 0;
+	int x;
+	for(x=0; x<sz; x++)
+	{
+		dst[x] = src[x];
+		if(src[x]== NULL)
+		{
+			break;
+		}
+	}
+	dst[sz-1]=0;
+	return x;
 }
 
 uint strncat(char* str1, char* str2, uint sz)
 {
-	return 0;
+	int x, y=0;
+	for(x=0; x<sz; x++)
+	{
+		if(str1[x]==NULL||x>strlen(str1))
+		{
+			if(str2[y]==NULL)
+			{
+				break;
+			}
+			str1[x] = str2[y];
+			y++;
+
+		}
+	}
+	str1[x-1]=0;
+	return strlen(str1);
+
+
 }
 
 int strcmp(char* str1, char* str2)
 {
-	return 0;
+	int x;
+	for(x=0; x < str1; x++)
+	{
+		if(str1[x]==str2[0])
+		{
+			return 0;
+		}
+		else if((str1[x]>=48 && str1[x] <= 57)&&(str2[x]>=48 && str2[x] <= 57))
+		{
+
+		}
+	}
+	return -1;
 }
 
 void memmove(void* dst, void* src, uint sz)
