cd25298db785d85cf9733babffc2fad63d324135
Max Strange <mbstrange@ip-172-31-33-225.us-west-2.compute.internal>
2015-05-26 02:46:07 +0000

    File System updates.

--- kernel/drivers/vsfs.c
+++ kernel/drivers/vsfs.c
@@ -61,7 +61,7 @@ int vsfs_lookup(char* path, vsfs_inode* dst)
         }
       }
     }
-    else if(root->blocks <= 81){
+    else if(root->blocks < 137){
 
     }
     else{
@@ -76,10 +76,9 @@ int vsfs_lookup(char* path, vsfs_inode* dst)
     uchar final_inode[512];
     ata_readsect(start + 1 + super->dmap + super->imap + (inode_num / 512), final_inode);
     vsfs_inode* ret_inode = ((vsfs_inode*) final_inode) + (inode_num % 8);
-    dst = ret_inode;
-    return 1;
+    memmove(dst, ret_inode, sizeof(vsfs_inode));
+    return inode_num;
   }
-
   else{
     return 0;
   }  
@@ -112,21 +111,261 @@ int vsfs_path_split(char* path, char** dst){
  * count of the file. If the file now has 0 links to it, free the file and
  * all of the blocks it is holding onto.
  */
-int vsfs_unlink(char* path){
+int vsfs_unlink(char* path)
+{
+  char parent_name[1024];
+  parent_path(path, parent_name);
+  vsfs_inode parent;
+  int parent_num = vsfs_lookup(parent_name, &parent);
 
-  vsfs_inode* unlinked;
+  vsfs_inode child;
+  int child_num = vsfs_lookup(path, &child);
   
-  if(vsfs_lookup(path, unlinked) == 1){
-    unlinked->links_count--;
-    if(unlinked->links_count == 0){
-      //free in the bitmap
-      //free data blocks in bitmap
-    }
+  directent last_entry;
 
+  directent last_entries[4];
+
+  read_block
+}
+
+int find_free_inode(){
+    
+  int return_num;
+  int i;
+  uchar bitmap[512];
+  for(i = 0; i < super->imap; i++){
+    ata_readsect(start + 1 + i, bitmap);
+    int j;
+    for(j = 0; j < 512; j++){
+      if(bitmap[j] != 0xFF){
+        int k;
+        for(k = 0; k < 8; k++){
+          if(((1 << k) & bitmap[j]) == 0){
+            bitmap[j] |= (1 << k);
+            ata_writesect(start + 1 + i, bitmap); 
+            return i*512 + j*8 + k;
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+int find_free_block(){
+
+  int return_num;
+  int i;
+  uchar bitmap[512];
+  for(i = 0; i < super->dmap; i++){
+    ata_readsect(start + 1 + i + super->imap, bitmap);
+    int j;
+    for(j = 0; j < 512; j++){
+      if(bitmap[j] != 0xFF){
+        int k;
+        for(k = 0; k < 8; k++){
+          if(((1 << k) & bitmap[j]) == 0){
+            bitmap[j] |= (1 << k);
+            uchar zero_sect[512];
+            memset(zero_sect, 0, 512);
+            ata_writesect(i*512 + j*8 + k, zero_sect);
+            ata_writesect(start + 1 + i + super->imap, bitmap);
+            return i*512 + j*8 + k;
+          }
+        }
+      }
+    }
+  }
+  return 0;
+
+}
+
+int trailing_slash(char* buff){
+
+  char* curr = buff;
+  
+  while(*(curr) != NULL){
+    curr++;
+  }
+  curr--;
+  while(*(curr) == '/'){
+    *(curr) = NULL;
+    curr--;
+  }
+  return 0;
+
+}
+
+int add_block_to_inode(vsfs_inode* inode, uint blocknum)
+{
+  int block_index = inode->size / 512;
+  
+  if(block_index < 9){
+   inode->direct[block_index] = blocknum;
+  }
+  else if(block_index < 137){
+    int ind_index = block_index - 9;
+    if(ind_index == 0){
+      inode->indirect = find_free_block();
+    }
+    uint indirect_block[128];
+    ata_readsect(inode->indirect, indirect_block);
+    indirect_block[ind_index] = blocknum;
+    ata_writesect(inode->indirect, indirect_block);
+  }
+  else if(block_index < 16521){
+    int ind_ind_index = block_index - 137; 
+    int index1 = ind_ind_index / 128;
+    int index2 = ind_ind_index % 128;
+    if(ind_ind_index == 0){
+      inode->double_indirect = find_free_block();
+    } 
+    if(ind_ind_index % 128 == 0){
+      uint double_block[128];
+      ata_readsect(inode->double_indirect, double_block);
+      double_block[index1] = find_free_block;
+      ata_writesect(inode->double_indirect, double_block);
+    }
+      uint dindirect[128];
+      uint indirect[128];
+      ata_readsect(inode->double_indirect, dindirect);
+      ata_readsect(dindirect[index1], indirect);
+      indirect[index2] = blocknum;
+      ata_writesect(dindirect[index1], indirect);
   }
   else{
-    return 0;
+    //gg
+    return -1;
   }
+  return 0;
+}
+
+int read_block(vsfs_inode* inode, uint block_index, void* dst)
+{
+  if(block_index < 9){
+   ata_readsect(inode->direct[block_index], dst);
+  }
+  else if(block_index < 137){
+    int ind_index = block_index - 9;
+    uint indirect_block[128];
+    ata_readsect(inode->indirect, indirect_block);
+    ata_readsect(indirect_block[ind_index], dst);
+  }
+  else if(block_index < 16521){
+    int ind_ind_index = block_index - 137; 
+    int index1 = ind_ind_index / 128;
+    int index2 = ind_ind_index % 128;
+    uint dindirect[128];
+    uint indirect[128];
+    ata_readsect(inode->double_indirect, dindirect);
+    ata_readsect(dindirect[index1], indirect);
+    ata_readsect(indirect[index2], dst);
+  }
+  else{
+    //gg
+    return -1;
+  }
+  return 0; 
+}
+
+int write_block(vsfs_inode* inode, uint block_index, void* src)
+{
+  if(block_index < 9){
+   ata_writesect(inode->direct[block_index], src);
+  }
+  else if(block_index < 137){
+    int ind_index = block_index - 9;
+    uint indirect_block[128];
+    ata_readsect(inode->indirect, indirect_block);
+    ata_writesect(indirect_block[ind_index], src);
+  }
+  else if(block_index < 16521){
+    int ind_ind_index = block_index - 137; 
+    int index1 = ind_ind_index / 128;
+    int index2 = ind_ind_index % 128;
+    uint dindirect[128];
+    uint indirect[128];
+    ata_readsect(inode->double_indirect, dindirect);
+    ata_readsect(dindirect[index1], indirect);
+    ata_writesect(indirect[index2], src);
+  }
+  else{
+    //gg
+    return -1;
+  }
+  return 0; 
+}
+
+
+int allocate_directent(vsfs_inode* parent, char* name, unint inode_num){
+
+  directent new_directory;
+
+  memmove(&new_directory.name, name, 124);
+  new_directory.inode_num = inode_num;
+
+  if(parent->size % 512 == 0){
+    int block_num = find_free_block();
+    add_block_to_inode(parent, block_num);  
+  }
+  directent entries[4];
+  read_block(parent, parent->size / 512 , entries); 
+
+  int i;
+  for(i = 0; i < (512 / sizeof(directent)); i++){
+    if(entries[i]->inode_num == 0){
+      break;
+    }
+  }
+  memmove(&entries[i], &new_directory, sizeof(directent));
+  write_block(parent, parent->size / 512, entries);
+
+  parent->size += sizeof(directent);
+
+}
+
+void write_inode(uint inode_num, vsfs_inode* inode)
+{
+  vsfs_inode inodes[512/sizeof(vsfs_inode)];
+  ata_readsect(start + 1 + super->imap + super->dmap + inode_num/(512/sizeof(vsfs_inode)), inodes);
+  uint offset = inode_num % (512/sizeof(vsfs_inode));
+  memmove(&inodes[offset], inode, sizeof(vsfs_inode));
+  ata_writesect(start + 1 + super->imap + super->dmap + inode_num/(512/sizeof(vsfs_inode)), inodes);
+}
+
+void read_inode(uint inode_num, vsfs_inode* inode)
+{
+  vsfs_inode inodes[512/sizeof(vsfs_inode)];
+  ata_readsect(start + 1 + super->imap + super->dmap + inode_num/(512/sizeof(vsfs_inode)), inodes);
+  uint offset = inode_num % (512/sizeof(vsfs_inode));
+  memmove(inode, &inodes[offset], sizeof(vsfs_inode));
+}
+
+void file_name(char *path, char* dst)
+{
+  while(*(path) != NULL){
+    path++;
+  }
+  while(*(path) != '/'){
+    path--;
+  }
+  path++;
+
+  memmove(dst, path, strlen(path));
+
+}
+
+void parent_path(char *path, char* dst)
+{
+  memmove(dst, path, strlen(path));
+
+  while(*(dst) != NULL){
+    dst++;
+  }
+  while(*(dst) != '/'){
+    dst--;
+  }
+  *(dst) = NULL;
 }
 
 /**
@@ -138,12 +377,33 @@ int vsfs_unlink(char* path){
 int vsfs_link(char* path, vsfs_inode* new_inode)
 {
   vsfs_inode* parent;
+  char* curr = path;
+  while(*(curr) != NULL){
+    curr++;
+  }
+  while(*(curr) != '/'){
+    curr--;
+  }
+  *(curr) = NULL;
 
-  if(vsfs_lookup(path, parent) == 1){
+  int parent_num = vsfs_lookup(path, parent);
+  if(parent_num != 0){
     // where to put directent
     // traverse to get next free inode
-    bit search =
-  
+    int inode_num = find_free_inode();
+    if(inode_num == 0){
+      return 1;
+    }
+    else{
+      curr++;
+      allocate_directent(parent, curr, inode_num);
+      new_inode->links_count = 1;
+      new_inode->size = 0;
+      new_inode->blocks = 0;
+      write_inode(parent_num, parent);
+      write_inode(inode_num, new_inode);
+      return 0;
+    }
   }
   else{
     return 1;
@@ -156,7 +416,25 @@ int vsfs_link(char* path, vsfs_inode* new_inode)
  */
 int vsfs_hard_link(char* new_file, char* link)
 {
-  // ?
+  vsfs_inode link_inode;
+  int link_num = vsfs_lookup(link, &link_inode);
+  if(link_num == 0) { return 1;}
+  char parent[1024];
+  parent_path(new_file, parent);
+
+  vsfs_inode parent_inode;
+
+  int parent_num = vsfs_lookup(parent, &parent_inode);
+  if(parent_num == 0) { return 1;}
+  char child[124];
+  file_name(new_file, child);
+  
+  allocate_directent(&parent_inode, child, link_num);
+
+  write_inode(parent_num, &parent_inode);
+  link_inode.links_count++;
+  write_inode(link_num, &link_inode);
+  return 0;
 }
 
 /**
@@ -165,6 +443,7 @@ int vsfs_hard_link(char* new_file, char* link)
 int vsfs_soft_link(char* new_file, char* link)
 {
   // ?
+  return 69;
 }
 
 /**
