76b59b2631d2ec9861e8070a262474b1b2de1697
Max Strange <mbstrange@ip-172-31-33-225.us-west-2.compute.internal>
2015-06-03 00:24:52 +0000

    More system calls.

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -16,7 +16,11 @@ int sys_fork(void)
 
 int sys_wait(int pid)
 {
-	return 0;
+  slock_acquire(&ptable_lock);
+  rproc->state = PROC_BLOCKED;
+  rproc->b_pid = pid;
+  scheduler();
+  return 0;
 }
 
 int sys_exec(const char* path, const char** argv)
@@ -28,16 +32,32 @@ void sys_exit(void)
 {
   int i;
   for(i = 0; i < MAXFILES; i++){
-    if(rproc->file_descriptors[i]->type != 0x00){
+    if(rproc->file_descriptors[i].type != 0x00){
       sys_close(i);
     }  
   }
+  rproc->state = PROC_KILLED; 
   sched();       	
 }
 
 int sys_open(const char* path)
 {
-	return 0;
+  int i;
+  int fd = -1;
+  for(i = 0; i < MAXFILES; i++){
+    if(rproc->file_descriptors[i].type == 0x00){
+      fd = i;
+    }
+  } 
+  if(fd == -1){
+    return -1;
+  }
+  rproc->file_descriptors[fd].type = FD_TYPE_FILE;
+  int inode = vsfs_lookup(path, &rproc->file_descriptors[fd].inode);
+  if(inode == 0){
+    return -1;
+  }
+  return fd;
 }
 
 int sys_close(int fd)
@@ -50,7 +70,13 @@ int sys_close(int fd)
 
 int sys_read(int fd, char* dst, uint sz)
 {
-	return 0;
+  if(rproc->file_descriptors[fd] == 0x00){
+    return -1;
+  }  
+  if(rproc->file_descriptors[fd].type == FD_TYPE_FILE){
+    vsfs_read(rproc->file_descriptors[fd].inode, dst, sz);
+  }
+  return 0;
 }
 
 int sys_write(int fd, char* dst, uint sz)
