db43d238295ec56846529403f2ba2a484982a2d7
Amber Arnold <alarnold2@wisc.edu>
2015-06-05 10:37:53 -0500

    started working on stdlock.c

--- lib/stdlock.c
+++ lib/stdlock.c
@@ -1,32 +1,35 @@
 #include "types.h"
 #include "stdlock.h"
 
+
 void slock_init(slock_t* lock)
 {
-
+	lock->val = 0;
 }
 
 void slock_acquire(slock_t* lock)
 {
-
+	//lock->val = 1;
 }
 
 void slock_release(slock_t* lock)
 {
-
+	lock->val = 0;
 }
 
 void tlock_init(tlock_t* lock)
 {
-
+	lock->next_ticket = 0;
+	lock->currently_serving = 0;
 }
 
 void tlock_acquire(tlock_t* lock)
 {
-
+	//lock->currently_serving = 1;
+	//lock->next_ticket+=1;
 }
 
 void tlock_release(tlock_t* lock)
 {
-
+	lock->currently_serving = 0;
 }
