181fcd017ba67feffce16c7bd135117ebd6b78e7
John Detter <jdetter@wisc.edu>
2016-07-18 17:18:56 -0500

    Fixing build issues

--- kernel/drivers/raid.c
+++ kernel/drivers/raid.c
@@ -5,7 +5,7 @@
 
 // #include <sys/types.h>
 #include <string.h>
-#include "kern/stdlib.h"
+#include "kstdlib.h"
 #include "file.h"
 #include "stdlock.h"
 #include "devman.h"
@@ -26,19 +26,18 @@ struct raid_context
 	int stride; /* How many sectors are in a stride? */
 	int stride_shift; /* Quick shift for stride */
 	/* Array of disk drivers (one for each disk) */
-	struct FSHardwareDriver* disks[RAID_MAX_DISKS];
+	struct StorageDevice* disks[RAID_MAX_DISKS];
 };
 
 slock_t raid_context_lock;
 struct raid_context raid_contexts[RAID_MAX_CONTEXT];
-struct FSHardwareDriver raids[RAID_MAX_CONTEXT];
+struct StorageDevice raids[RAID_MAX_CONTEXT];
 
-static struct FSHardwareDriver* raid_alloc(void)
+static struct StorageDevice* raid_alloc(void)
 {
-	struct FSHardwareDriver* driver = NULL;
+	struct StorageDevice* driver = NULL;
 
 	slock_acquire(&raid_context_lock);
-
 	int x;
 	for(x = 0;x < RAID_MAX_CONTEXT;x++)
 	{
@@ -50,13 +49,12 @@ static struct FSHardwareDriver* raid_alloc(void)
 			break;
 		}
 	}
-
 	slock_release(&raid_context_lock);
 
 	return driver;
 }
 
-static void raid_free(struct FSHardwareDriver* driver)
+static void raid_free(struct StorageDevice* driver)
 {
 	if(!driver) return;
 	slock_acquire(&raid_context_lock);
@@ -67,86 +65,85 @@ static void raid_free(struct FSHardwareDriver* driver)
 int raid_init(void)
 {
 	slock_init(&raid_context_lock);
-	memset(raids, 0, sizeof(struct raid_context) * RAID_MAX_CONTEXT);
-	memset(raids, 0, sizeof(struct FSHardwareDriver) * RAID_MAX_CONTEXT);
+	memset(raid_contexts, 0, sizeof(struct raid_context) * RAID_MAX_CONTEXT);
+	memset(raids, 0, sizeof(struct StorageDevice) * RAID_MAX_CONTEXT);
 	return 0;
 }
 
-struct FSHardwareDriver* raid0_init(struct FSHardwareDriver** drivers, 
+struct StorageDevice* raid0_init(struct StorageDevice** drivers, 
 		int driver_count, int stride)
 {
-	struct FSHardwareDriver* driver = raid_alloc();
+	struct StorageDevice* driver = raid_alloc();
 	/* did we actually get a driver? */
 	if(!driver) return NULL;
 
 	/* Create shortcut for the context */
 	struct raid_context* context = driver->context;
 
-	int min_sect = -1;
+	int sectors = -1;
 	/* Do the disks not all have the same sectsize? */
 	int sects_differ = 0;
 	int sect_size = -1;
 	int sect_shift = -1;
 
-        int x;
-        for(x = 0;x < driver_count;x++)
-        {
-                context->disks[x] = drivers[x];
-                if(drivers[x]->sectmax < min_sect || min_sect == -1)
-                        min_sect = drivers[x]->sectmax;
-                if(sect_size == -1)
-                        sect_size = drivers[x]->sectsize;
-                if(sect_shift == -1)
-                        sect_shift = drivers[x]->sectshifter;
+	int x;
+	for(x = 0;x < driver_count;x++)
+	{
+		context->disks[x] = drivers[x];
+		if(drivers[x]->sectors < sectors || sectors == -1)
+			sectors = drivers[x]->sectors;
+		if(sect_size == -1)
+			sect_size = drivers[x]->sectsize;
+		if(sect_shift == -1)
+			sect_shift = drivers[x]->sectshifter;
+		if(sect_size != drivers[x]->sectsize
+				|| sect_shift != drivers[x]->sectshifter)
+			sects_differ = 1;
 
-                if(sect_size != drivers[x]->sectsize
-                                || sect_shift != drivers[x]->sectshifter)
-                        sects_differ = 1;
+	}
 
-        }
-
-        if(sects_differ)
-        {
+	if(sects_differ)
+	{
 #ifdef DEBUG
-                panic("raid: disks do not have the same sector size!\n");
+		panic("raid: disks do not have the same sector size!\n");
 #endif
-                raid_free(driver);
-                return NULL;
-        }
+		raid_free(driver);
+		return NULL;
+	}
 
-        context->disk_count = driver_count;
-        driver->valid = 1;
-        // driver->readsect = raid0_readsect;
-        // if(0) driver->readsects = raid0_readsects;
-        // driver->writesect = raid0_writesect;
-        // if(0) driver->writesects = raid0_writesects;
-        driver->sectmax = min_sect;
-        driver->sectshifter = sect_shift;
-        driver->sectsize = sect_size;
+	context->disk_count = driver_count;
+	driver->valid = 1;
+	// driver->readsect = raid0_readsect;
+	// if(0) driver->readsects = raid0_readsects;
+	// driver->writesect = raid0_writesect;
+	// if(0) driver->writesects = raid0_writesects;
+	driver->sectors = sectors;
+	driver->sectshifter = sect_shift;
+	driver->sectsize = sect_size;
 
 	/* Set the stride equal to the page size */
 	context->stride = stride;
 	context->stride_shift = log2(stride);
 
-        /* Setup the cache manager for this driver */
-        memset(&driver->cache, 0, sizeof(struct cache));
+	/* Setup the cache manager for this driver */
+	memset(&driver->cache, 0, sizeof(struct cache));
 
-        return driver;
+	return driver;
 }
 
-int raid0_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid)
+int raid0_setup_cache(struct FSDriver* driver, struct StorageDevice* raid)
 {
-        if(!driver || !raid) return -1;
-        // driver->reference = raid0_cache_reference;
-        // driver->addreference = raid0_cache_addreference;
-        // driver->dereference = raid0_cache_dereference;
-        return 0;
+	if(!driver || !raid) return -1;
+	// driver->reference = raid0_cache_reference;
+	// driver->addreference = raid0_cache_addreference;
+	// driver->dereference = raid0_cache_dereference;
+	return 0;
 }
 
 /** VVV TODO: Start implementing here VVV*/
 
 int raid0_readsect(void* dst, sect_t sect, 
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	struct raid_context* context = driver->context;
 
@@ -160,7 +157,7 @@ int raid0_readsect(void* dst, sect_t sect,
 }
 
 int raid0_readsects(void* dst, sect_t sectstart, int sectcount, 
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	int current = 0;//postion from sectstart
 
@@ -175,7 +172,7 @@ int raid0_readsects(void* dst, sect_t sectstart, int sectcount,
 }
 
 int raid0_writesect(void* src, sect_t sect, 
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	struct raid_context* context = driver->context;
 
@@ -189,7 +186,7 @@ int raid0_writesect(void* src, sect_t sect,
 }
 
 int raid0_writesects(void* src, sect_t sectstart, int sectcount, 
-		struct FSHardwareDriver* driver) 
+		struct StorageDevice* driver) 
 {
 
 	int current = 0;//postion from sectstart
@@ -207,14 +204,14 @@ int raid0_writesects(void* src, sect_t sectstart, int sectcount,
 
 void* raid0_cache_reference(int id, struct FSDriver* driver)
 {
-	struct FSHardwareDriver* raid = driver->driver;
+	struct StorageDevice* raid = driver->driver;
 	struct raid_context* context = raid->context;
 
 	blk_t local_id = (blk_t)id;
-        /* Convert this from the drivers view to the global view */
-        int block_to_sect = driver->blockshift - driver->driver->sectshifter;
-        /* Which start sector is this on disk? */
-        sect_t gbl_sect = (local_id << block_to_sect) + driver->start;
+	/* Convert this from the drivers view to the global view */
+	int block_to_sect = driver->blockshift - driver->driver->sectshifter;
+	/* Which start sector is this on disk? */
+	sect_t gbl_sect = (local_id << block_to_sect) + driver->start;
 
 	/* Which disk is this on? */
 	int disk_num = -1;
@@ -230,10 +227,10 @@ void* raid0_cache_reference(int id, struct FSDriver* driver)
 	} else {
 		disk_num = (gbl_sect / context->stride) 
 			% context->disk_count;
-                int local_stride = (gbl_sect / context->stride) 
+		int local_stride = (gbl_sect / context->stride) 
 			/ context->disk_count;
-                local_sect = (local_stride * context->stride)
-                        + gbl_sect % context->stride;
+		local_sect = (local_stride * context->stride)
+			+ gbl_sect % context->stride;
 	}
 
 #ifdef DEBUG
@@ -241,26 +238,26 @@ void* raid0_cache_reference(int id, struct FSDriver* driver)
 		panic("raid0: invalid sector!\n");
 #endif
 
-	struct FSHardwareDriver* disk = context->disks[disk_num];
+	struct StorageDevice* disk = context->disks[disk_num];
 
-        /* Save the original requested block_id */
-        sect_t sect_start = local_sect;
-        /* Round global sect down to a page boundary */
-        local_sect &= ~(disk->spp - 1);
-        /* How many blocks were lost when we rounded down? */
-        int diff = (sect_start - gbl_sect) >> block_to_sect;
+	/* Save the original requested block_id */
+	sect_t sect_start = local_sect;
+	/* Round global sect down to a page boundary */
+	local_sect &= ~(disk->spp - 1);
+	/* How many blocks were lost when we rounded down? */
+	int diff = (sect_start - gbl_sect) >> block_to_sect;
 
-        /* reference the blocks - pointer points to start of page */
-        char* bp = cache_reference(local_sect,
-                        &driver->driver->cache, driver);
+	/* reference the blocks - pointer points to start of page */
+	char* bp = cache_reference(local_sect,
+			&driver->driver->cache, driver);
 
-        /* Adjust the pointer to point at the requested space */
-        bp += diff << driver->blockshift;
+	/* Adjust the pointer to point at the requested space */
+	bp += diff << driver->blockshift;
 
 #ifdef DEBUG
 	cprintf("raid0: reference for global %d\n", global_id);
 	cprintf("raid0: this is local %d on disk %d\n", block_id_start, 
-		disk_num);
+			disk_num);
 	cprintf("raid0: block_start: %d actual: %d diff: %d\n",
 			block_id_start, local_id, diff);
 	cprintf("raid0: returned pointer: 0x%x\n", bp);
@@ -274,11 +271,11 @@ void* raid0_cache_addreference(int id,
 		struct FSDriver* driver)
 {
 	blk_t global_id = (blk_t)id;
-        struct FSHardwareDriver* raid = driver->driver;
-        struct raid_context* context = raid->context;
-        int disk_num = global_id % context->disk_count;
-        int local_id = (global_id - disk_num) / context->disk_count;
-        struct FSHardwareDriver* disk = context->disks[disk_num];
+	struct StorageDevice* raid = driver->driver;
+	struct raid_context* context = raid->context;
+	int disk_num = global_id % context->disk_count;
+	int local_id = (global_id - disk_num) / context->disk_count;
+	struct StorageDevice* disk = context->disks[disk_num];
 
 	uint block_id_start = local_id;
 	local_id &= ~(driver->bpp - 1);
@@ -300,43 +297,43 @@ void* raid0_cache_addreference(int id,
 int raid0_cache_dereference(void* ref, struct FSDriver* driver)
 {
 	if(!ref) return 0;
-        struct FSHardwareDriver* raid = driver->driver;
-        struct raid_context* context = raid->context;
-        struct FSHardwareDriver* disk = NULL;
+	struct StorageDevice* raid = driver->driver;
+	struct raid_context* context = raid->context;
+	struct StorageDevice* disk = NULL;
 
-        /* Which disk cache does this ref belong to? */
-        int x;
-        for(x = 0;x < context->disk_count;x++)
-        {
-                disk = context->disks[x];
-                if(!context->disks[x]->cache.slab_shift)
-                        panic("Bad raid cache!\n");
-                int slabs_sz = disk->cache.entry_count
-                        << disk->cache.slab_shift;
-                if((char*)ref >= disk->cache.slabs &&
-                                (char*)ref < disk->cache.slabs + slabs_sz)
-                        break;
-        }
-        
+	/* Which disk cache does this ref belong to? */
+	int x;
+	for(x = 0;x < context->disk_count;x++)
+	{
+		disk = context->disks[x];
+		if(!context->disks[x]->cache.slab_shift)
+			panic("Bad raid cache!\n");
+		int slabs_sz = disk->cache.entry_count
+			<< disk->cache.slab_shift;
+		if((char*)ref >= disk->cache.slabs &&
+				(char*)ref < disk->cache.slabs + slabs_sz)
+			break;
+	}
+
 	/* ref must be rounded to a page boundary */
 	char* ref_c = ref;
-        ref_c = (char*)((uint)ref_c & ~(PGSIZE - 1));
+	ref_c = (char*)((uint)ref_c & ~(PGSIZE - 1));
 
 #ifdef DEBUG
 	cprintf("raid0: dereferencing 0x%x\n", ref);
 	cprintf("raid0: disk for this pointer: 0x%x\n", x);
 #endif
 
-        /* Invalid pointer */
-        if(x >= context->disk_count) return -1;
+	/* Invalid pointer */
+	if(x >= context->disk_count) return -1;
 
-        return cache_dereference(ref_c, &disk->cache, disk);
+	return cache_dereference(ref_c, &disk->cache, disk);
 }
 
-struct FSHardwareDriver* raid1_init(struct FSHardwareDriver** drivers, 
+struct StorageDevice* raid1_init(struct StorageDevice** drivers, 
 		int driver_count, int stride)
 {
-	struct FSHardwareDriver* driver = raid_alloc();
+	struct StorageDevice* driver = raid_alloc();
 	/* did we actually get a driver? */
 	if(!driver) return NULL;
 
@@ -363,7 +360,7 @@ struct FSHardwareDriver* raid1_init(struct FSHardwareDriver** drivers,
 		if(sect_size != drivers[x]->sectsize
 				|| sect_shift != drivers[x]->sectshifter)
 			sects_differ = 1;
-			
+
 	}
 
 	if(sects_differ)
@@ -392,7 +389,7 @@ struct FSHardwareDriver* raid1_init(struct FSHardwareDriver** drivers,
 	return driver;
 }
 
-int raid1_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid)
+int raid1_setup_cache(struct FSDriver* driver, struct StorageDevice* raid)
 {
 	if(!driver || !raid) return -1;
 	// driver->reference = raid1_cache_reference;
@@ -402,7 +399,7 @@ int raid1_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid)
 }
 
 int raid1_readsect(void* dst, sect_t sect,
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	struct raid_context* context = driver->context;
 
@@ -417,7 +414,7 @@ int raid1_readsect(void* dst, sect_t sect,
 }
 
 int raid1_readsects(void* dst, sect_t sectstart, int sectcount,
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 
 	int current = 0;//postion from sectstart
@@ -433,7 +430,7 @@ int raid1_readsects(void* dst, sect_t sectstart, int sectcount,
 }
 
 int raid1_writesect(void* src, sect_t sect, 
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 
 	struct raid_context* context = driver->context;
@@ -450,7 +447,7 @@ int raid1_writesect(void* src, sect_t sect,
 }
 
 int raid1_writesects(void* src, sect_t sectstart, int sectcount,
-		struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	int current = 0;//postion from sectstart
 
@@ -467,9 +464,9 @@ int raid1_writesects(void* src, sect_t sectstart, int sectcount,
 void* raid1_cache_reference(int id, struct FSDriver* driver)
 {
 	blk_t block_id = (blk_t)id;
-	struct FSHardwareDriver* raid = driver->driver;
+	struct StorageDevice* raid = driver->driver;
 	struct raid_context* context = raid->context;
-	struct FSHardwareDriver* disk = context->disks[0];
+	struct StorageDevice* disk = context->disks[0];
 	/* Just read from disk 1 */
 	/* Save the original requested block_id */
 	uint block_id_start = block_id;
@@ -491,9 +488,9 @@ void* raid1_cache_reference(int id, struct FSDriver* driver)
 void* raid1_cache_addreference(int id, struct FSDriver* driver)
 {
 	blk_t block_id = (blk_t)id;
-	struct FSHardwareDriver* raid = driver->driver;
+	struct StorageDevice* raid = driver->driver;
 	struct raid_context* context = raid->context;
-	struct FSHardwareDriver* disk = context->disks[0];
+	struct StorageDevice* disk = context->disks[0];
 
 	uint block_id_start = block_id;
 	block_id &= ~(driver->bpp - 1);
@@ -508,9 +505,9 @@ void* raid1_cache_addreference(int id, struct FSDriver* driver)
 int raid1_cache_dereference(void* ref, struct FSDriver* driver)
 {
 	if(!ref) return 0;
-	struct FSHardwareDriver* raid = driver->driver;
+	struct StorageDevice* raid = driver->driver;
 	struct raid_context* context = raid->context;
-	struct FSHardwareDriver* disk = context->disks[0];
+	struct StorageDevice* disk = context->disks[0];
 
 	/* ref must be rounded to a page boundary */
 	char* ref_c = ref;
@@ -519,10 +516,10 @@ int raid1_cache_dereference(void* ref, struct FSDriver* driver)
 	return cache_dereference(ref_c, &disk->cache, disk);
 }
 
-struct FSHardwareDriver* raid5_init(struct FSHardwareDriver** drivers,
-                int driver_count, int stride)
+struct StorageDevice* raid5_init(struct StorageDevice** drivers,
+		int driver_count, int stride)
 {
-	struct FSHardwareDriver* driver = raid_alloc();
+	struct StorageDevice* driver = raid_alloc();
 	/* did we actually get a driver? */
 	if(!driver) return NULL;
 
@@ -549,7 +546,7 @@ struct FSHardwareDriver* raid5_init(struct FSHardwareDriver** drivers,
 		if(sect_size != drivers[x]->sectsize
 				|| sect_shift != drivers[x]->sectshifter)
 			sects_differ = 1;
-			
+
 	}
 
 	if(sects_differ)
@@ -580,38 +577,38 @@ struct FSHardwareDriver* raid5_init(struct FSHardwareDriver** drivers,
 	return driver;
 }
 
-int raid5_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid)
+int raid5_setup_cache(struct FSDriver* driver, struct StorageDevice* raid)
 {
-        if(!driver || !raid) return -1;
-        // driver->reference = raid5_cache_reference;
-        // driver->addreference = raid5_cache_addreference;
-        // driver->dereference = raid5_cache_dereference;
-        return 0;
+	if(!driver || !raid) return -1;
+	// driver->reference = raid5_cache_reference;
+	// driver->addreference = raid5_cache_addreference;
+	// driver->dereference = raid5_cache_dereference;
+	return 0;
 }
 
 int raid5_readsect(void* dst, sect_t sect,
-                struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	/* TODO: implementation needed */
 	return -1;
 }
 
 int raid5_readsects(void* dst, sect_t sectstart, int sectcount,
-                struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	/* TODO: implementation needed */
 	return -1;
 }
 
 int raid5_writesect(void* src, sect_t sect,
-                struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	/* TODO: implementation needed */
 	return -1;
 }
 
 int raid5_writesects(void* src, sect_t sectstart, int sectcount,
-                struct FSHardwareDriver* driver)
+		struct StorageDevice* driver)
 {
 	/* TODO: implementation needed */
 	return -1;
@@ -623,7 +620,7 @@ void* raid5_cache_reference(int block_id, struct FSDriver* driver)
 }
 
 void* raid5_cache_addreference(int block_id,
-                struct FSDriver* driver)
+		struct FSDriver* driver)
 {
 	return NULL;
 }
--- kernel/include/raid.h
+++ kernel/include/raid.h
@@ -12,39 +12,39 @@ int raid_init(void);
  * Initliize the raid driver with the array of drivers. driver_count is the
  * amount of drivers in the array. Returns 0 on success.
  */
-struct FSHardwareDriver* raid0_init(struct FSHardwareDriver** drivers,
+struct StorageDevice* raid0_init(struct StorageDevice** drivers,
 	int driver_count, int stride);
 
 /**
  * Initilize the raid driver with the array of drivers. driver_count is the
  * amount of drivers in the array. Returns 0 on success.
  */
-int raid0_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid);
+int raid0_setup_cache(struct FSDriver* driver, struct StorageDevice* raid);
 
 /**
  * Initilize the raid driver with the array of drivers. driver_count is the
  * amount of drivers in the array. Returns 0 on success.
  */
-struct FSHardwareDriver* raid1_init(struct FSHardwareDriver** drivers,
+struct StorageDevice* raid1_init(struct StorageDevice** drivers,
 	int driver_count, int stride);
 
 /**
  * Setup an FSDriver to use the raid specific disk cache. Returns 0
  * on success, nonzero otherwise.
  */
-int raid1_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid);
+int raid1_setup_cache(struct FSDriver* driver, struct StorageDevice* raid);
 
 /**
  * Initilize the raid driver with the array of drivers. driver_count is the
  * amount of drivers in the array. Returns 0 on success.
  */
-struct FSHardwareDriver* raid5_init(struct FSHardwareDriver** drivers,
+struct StorageDevice* raid5_init(struct StorageDevice** drivers,
         int driver_count, int stride);
 
 /**
  * Setup an FSDriver to use the raid specific disk cache. Returns 0
  * on success, nonzero otherwise.
  */
-int raid5_setup_cache(struct FSDriver* driver, struct FSHardwareDriver* raid);
+int raid5_setup_cache(struct FSDriver* driver, struct StorageDevice* raid);
 
 #endif
